015e309d2b0c39907eb250e3a627a7e4fcdd7163
mdecano07 <48596724+mdecano07@users.noreply.github.com>
2023-10-16 07:43:40 +0100

    update LoanService

--- src/main/java/com/berenberg/InventoryTracker/service/LoanService.java
+++ src/main/java/com/berenberg/InventoryTracker/service/LoanService.java
@@ -2,6 +2,8 @@ package com.berenberg.InventoryTracker.service;
 
 import com.berenberg.InventoryTracker.model.Loan;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,26 +14,40 @@ import java.util.stream.Collectors;
 @Service
 @AllArgsConstructor
 public class LoanService {
+    private final Logger logger = LoggerFactory.getLogger(DataService.class);
+
     @Autowired
     DataService dataService;
 
     //method to allow a user to borrow multiple items
     public void borrowItems(Set<Integer> uniqueItemIds, Integer userId) {
-        final var loanStartDate = LocalDate.now();
+        if (userId == null) {
+            logger.error("User id is null. Please populate this field");
+        } else if (uniqueItemIds == null) {
+            logger.error("UniqueItemIds is null. Please populate this field");
+        } else {
+            final var loanStartDate = LocalDate.now();
 
-        final var loansList = uniqueItemIds.stream()
-                .map(uniqueItemId -> new Loan(userId, uniqueItemId, loanStartDate))
-                .collect(Collectors.toList());
+            final var loansList = uniqueItemIds.stream()
+                    .map(uniqueItemId -> new Loan(userId, uniqueItemId, loanStartDate))
+                    .collect(Collectors.toList());
 
-        dataService.addLoanData(loansList);
+            dataService.addLoanData(loansList);
+        }
     }
 
     public void returnItems(Set<Integer> uniqueItemIds, Integer userId) {
+        if (userId == null) {
+            logger.error("User id is null. Please populate this field");
+        } else if (uniqueItemIds == null) {
+            logger.error("UniqueItemIds is null. Please populate this field");
+        } else {
         //get the existing loans for user and uniqueItemIds and remove them
         final var loansToRemove = dataService.getLoanData().stream()
                 .filter(loan -> userId.equals(loan.getUserId()) && uniqueItemIds.contains(loan.getUniqueItemId()))
                 .collect(Collectors.toList());
 
         dataService.removeLoanData(loansToRemove);
+        }
     }
 }
